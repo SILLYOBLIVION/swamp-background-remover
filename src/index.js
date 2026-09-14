794c48136884676f9763d4fb741fd3376e09367e
tyler mcpherson <tylersnerfandstuff@gmail.com>
2026-09-14 00:49:38 -0500

    fix model proxy headers

--- src/index.js
+++ src/index.js
@@ -3,18 +3,47 @@ export default {
         const url = new URL(request.url);
 
         if (url.pathname.startsWith("/hf/")) {
+            if (request.method === "OPTIONS") {
+                return new Response(null, {
+                    status: 204,
+                    headers: {
+                        "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Methods": "GET, HEAD, OPTIONS",
+                        "Access-Control-Allow-Headers": "*"
+                    }
+                });
+            }
+
             const hfPath = url.pathname.slice(4);
 
             const hfURL =
-                "https://huggingface.co/" + hfPath + url.search;
+                "https://huggingface.co/" +
+                hfPath +
+                url.search;
 
             const response = await fetch(hfURL, {
                 method: request.method,
-                headers: request.headers,
                 redirect: "follow"
             });
 
-            const headers = new Headers(response.headers);
+            const headers = new Headers();
+
+            const contentType =
+                response.headers.get("Content-Type");
+
+            if (contentType) {
+                headers.set("Content-Type", contentType);
+            }
+
+            const contentLength =
+                response.headers.get("Content-Length");
+
+            if (contentLength) {
+                headers.set(
+                    "Content-Length",
+                    contentLength
+                );
+            }
 
             headers.set(
                 "Access-Control-Allow-Origin",
@@ -35,7 +64,6 @@ export default {
                 response.body,
                 {
                     status: response.status,
-                    statusText: response.statusText,
                     headers
                 }
             );
